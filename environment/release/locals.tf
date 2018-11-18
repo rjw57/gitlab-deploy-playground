4771bf50a2b234c8521b0bc13910248f68789606
Rich Wareham <rich@richwareham.com>
2018-11-18 22:23:53 +0000

    run terraform fmt

--- environment/release/locals.tf
+++ environment/release/locals.tf
@@ -7,10 +7,10 @@ locals {
   db_username = "${google_sql_user.gitlab.name}"
   db_password = "${google_sql_user.gitlab.password}"
 
-  db_password_secret           = "${kubernetes_secret.db_password.metadata.0.name}"
-  db_proxy_credentials_secret  = "${kubernetes_secret.db_proxy_credentials.metadata.0.name}"
-  backups_s3cfg_secret         = "${kubernetes_secret.backups_s3cfg.metadata.0.name}"
-  saml_config_secret           = "${kubernetes_secret.saml_config.metadata.0.name}"
+  db_password_secret          = "${kubernetes_secret.db_password.metadata.0.name}"
+  db_proxy_credentials_secret = "${kubernetes_secret.db_proxy_credentials.metadata.0.name}"
+  backups_s3cfg_secret        = "${kubernetes_secret.backups_s3cfg.metadata.0.name}"
+  saml_config_secret          = "${kubernetes_secret.saml_config.metadata.0.name}"
 
   # Actual domain we pass to the gitlab chart. Can be overridden.
   gitlab_domain = "${var.gitlab_domain == "" ? replace(var.dns_name, "/\\.$/", "") : var.gitlab_domain}"
